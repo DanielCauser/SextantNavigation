2955cf63d55ffa04c89026b7e204a520e7451128
Daniel Causer <dcauser@investcloud.com>
2024-10-10 17:03:47 -0400

    clean up

--- SextantNavigation/LoginPage.xaml.cs
+++ SextantNavigation/LoginPage.xaml.cs
@@ -10,9 +10,6 @@ public partial class LoginPage : ContentPage
 
         Logintbtn.Command = ReactiveCommand.CreateFromTask(() =>
         {
-            var loginPage = App.NavigationManager.Navigation.NavigationStack[0];
-
-
             App.NavigationManager.Navigation.InsertPageBefore(new MainPage(), App.NavigationManager.Navigation.NavigationStack[0]);
             return App.NavigationManager.Navigation.PopToRootAsync(false);
         });
